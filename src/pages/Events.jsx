a1b65681954704241e28466e56009a045c0ff8db
Animesh Singh <87444276+Animesh0764@users.noreply.github.com>
2024-02-10 14:12:18 +0530

    m

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -9,23 +9,22 @@ const Events = () => {
 
     const responsive = {
         superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5,
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3,
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2,
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-        },
-      };
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5,
+            },
+            desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+            },
+            tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+            },
+            mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            },
+        };
 
     const ongoingEvent = ongoing.map(event =>(
         <EventCard {...event} />
